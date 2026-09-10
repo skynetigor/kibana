9067ff91f42df99cd467f028aabf344976f4d36a
Ihor Panasiuk <igorskynet13@gmail.com>
2026-09-10 16:02:00 +0200

    fix contract

--- src/platform/plugins/shared/workflows_execution_engine/server/workflow_context_manager/workflow_execution_runtime_manager.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/workflow_context_manager/workflow_execution_runtime_manager.ts
@@ -138,7 +138,7 @@ export class WorkflowExecutionRuntimeManager {
     this.workflowExecutionCursor.navigateToAfterNode(nodeId);
   }
 
-  public navigateToSynthetic(params: { stepId: string; stepType?: string }): void {
+  public navigateToSynthetic(params: { stepId: string; stepType: string }): void {
     this.workflowExecutionCursor.navigateToSynthetic(params);
   }
 
